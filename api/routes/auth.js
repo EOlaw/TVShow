776e7cf18e583b99b8673769370895d0aef23d41
EOlaw <Emmanuel.ao@outlook.com>
2022-10-26 02:03:31 -0500

    minor update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
+var CryptoJS = require("crypto-js")
 
 //REGISTER
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password
+        password:CryptoJS.AES.encrypt(req.body.password, 'secre-war').toString(),
     });
-
+    console.log(newUser)
     try{
         const user = await newUser.save();
         res.status(201).json(user);
