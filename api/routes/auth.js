abda51ef13821065eddc875724bd37d537d97c73
EOlaw <Emmanuel.ao@outlook.com>
2022-10-27 14:46:53 -0500

    updated the server

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
-var CryptoJS = require("crypto-js")
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -29,11 +30,16 @@ router.post("/login", async (req, res) => {
         const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
 
-        originalPassword !== req.body.password && res.status(401).json("Wrong password!");
-        const {password, ...info } = user._doc;
+        originalPassword !== req.body.password && 
+        res.status(401).json("Wrong password!");
+
+        const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, 
+            'accessToken', { expiresIn: "5d" });
+
+        const { password, ...info } = user._doc;
 
 
-        res.status(200).json(info);
+        res.status(200).json({ ...info, accessToken });
     }catch(err){
         res.status(500).json(err)
     }
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
 
 
 //Database Connection
@@ -15,7 +18,10 @@ db.once("open", () => {
 app.use(express.json());
 
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 
 
